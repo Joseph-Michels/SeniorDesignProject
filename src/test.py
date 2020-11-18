1838d6737e9c079d6e0569c525914199dd3f716f
Joseph Michels <bobelement4181@gmail.com>
2020-11-17 16:13:11 -0800

    load encodings to files

--- src/test.py
+++ src/test.py
@@ -3,18 +3,23 @@ import numpy as np
 from pathlib import Path
 
 if __name__ == "__main__":
+    names = []
     imgs = []
     encs = []
+    N = 0
+    
     for img_path in Path("img").iterdir():
+        s = str(img_path)
+        names.append( s[s.find('\\')+1:s.find('.')] )
         imgs.append( frec.load_image_file(img_path) )
         encs.append( frec.face_encodings(imgs[-1])[0] )
-    imgs = np.array(imgs)
+        N += 1
+
     encs = np.array(encs)
 
-    print(imgs.shape)
-    print(encs.shape)
+    print(f"Each encoding has {encs.shape[1]} values")
 
-    print("Distances")
-    print(f"Obama 1 / Obama 2: {np.linalg.norm(encs[2] - encs[1])}")
-    print(f"Obama 1 / Biden: {np.linalg.norm(encs[2] - encs[0])}")
-    print(f"Obama 2 / Biden: {np.linalg.norm(encs[2] - encs[0])}")
+    print("Distances:")
+    for i in range(N):
+        for j in range(i):
+            print(f"  {names[j]} to {names[i]}: {np.linalg.norm(encs[j]-encs[i])}")
