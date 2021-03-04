46d179cb441a04e88de24ba4169f0ea4220de092
Joseph Michels <bobelement4181@gmail.com>
2021-03-04 01:52:58 -0800

    added baud for actual drone connection

--- src/drone_to_face.py
+++ src/drone_to_face.py
@@ -131,7 +131,7 @@ def connect_drone():
 
     # Connect to the Vehicle
     print('Connecting to vehicle on: %s' % connection_string)
-    vehicle = connect(connection_string, wait_ready=True)
+    vehicle = connect(connection_string, baud=57600, wait_ready=True)
 
     return vehicle
 
